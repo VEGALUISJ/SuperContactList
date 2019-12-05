bc93401f8299c963f0fb435d1b0d5e9d8f8fde1b
Jose Luis Vega <Luisvega.jlvv@gmail.com>
2019-12-05 21:47:13 +0000

    vers 1

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,8 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { LogIn } from "./views/login";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -23,8 +22,6 @@ export const Layout = () => {
 					<Navbar />
 					<Switch>
 						<Route exact path="/" component={Home} />
-						<Route path="/demo" component={Demo} />
-						<Route path="/single/:theid" component={Single} />
 						<Route render={() => <h1>Not found!</h1>} />
 					</Switch>
 					<Footer />
---
+++ src/js/views/login.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class LogIn extends Component {
+	render() {
+		return (
+			<div className="contanier-fluid containerlogin">
+				<div className="row mx-auto col-sm-10">
+					<div className="offset-md-4 col-md-4">
+						<h2>LOGIN</h2>
+
+						<form action="">
+							<div className="form-group">
+								<label htmlFor="">Username</label>
+								<input type="text" className="form-control" />
+							</div>
+
+							<div className="form-group">
+								<label htmlFor="">Password</label>
+								<input type="password" className="form-control" />
+							</div>
+							<button type="button" className="btn btn-outline-primary">
+								Login
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
