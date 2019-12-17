374f66ebbb0680cc1e5dea8d2b317a0bbc7641c2
Jose Luis Vega <Luisvega.jlvv@gmail.com>
2019-12-17 18:47:24 +0000

    vers 2

--- src/js/views/addcontact.js
+++ src/js/views/addcontact.js
@@ -4,9 +4,15 @@ import { Link } from "react-router-dom";
 export class AddContact extends Component {
 	render() {
 		return (
-			<di>
-				<p>test</p>
-			</di>
+			<div>
+				<div className="container">
+					<h1 className="text-center mt-4">Add a New Contact</h1>
+					<div className="form-group">
+						<label htmlFor="exampleInputEmail1">Full Name</label>
+						<input type="Name" className="form-control" id="InputFullname" placeholder="Enter Full Name" />
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
